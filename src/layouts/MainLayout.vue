8bba4a7a3fa40aa4b0d78fa7e7e45b19e85302c7
olaalao007-png <ola.alao007@gmail.com>
2025-11-25 00:12:50 +0100

    corrected the app dashboard's displaying

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,7 +2,7 @@
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar>
-        <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
+      <!--  <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" /> -->
 
         <q-toolbar-title> Printemps Register </q-toolbar-title>
 
@@ -10,13 +10,13 @@
       </q-toolbar>
     </q-header>
 
-    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+   <!-- <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
       <q-list>
         <q-item-label header> Essential Links </q-item-label>
 
         <EssentialLink v-for="link in linksList" :key="link.title" v-bind="link" />
-      </q-list>
-    </q-drawer>
+      </q-list> 
+    </q-drawer> -->
 
     <q-page-container>
       <router-view />
@@ -24,9 +24,9 @@
   </q-layout>
 </template>
 
-<script>
+<!-- <script>
 import { defineComponent, ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
+//import EssentialLink from 'components/EssentialLink.vue'
 
 const linksList = [
   {
@@ -92,4 +92,4 @@ export default defineComponent({
     }
   },
 })
-</script>
+</script> -->
